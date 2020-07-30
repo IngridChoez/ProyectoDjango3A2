d20ea7359fe6761fb37193e68b54e628d33e66b9
GENERATION 10 <iechoez@est.itsgg.edu.ec>
2020-07-30 01:21:00 -0500

    cambio en   github

--- ProyectoDjango3A/urls.py
+++ ProyectoDjango3A/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about-me"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
